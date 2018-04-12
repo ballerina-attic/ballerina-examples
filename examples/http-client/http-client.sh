6e33b762f1f8af97f0d9010d60987f9fb074b408
bhashinee <bhashineen@gmail.com>
2018-04-12 11:03:09 +0530

    Rename server to listener

--- examples/http-client/http-client.sh
+++ examples/http-client/http-client.sh
@@ -1,4 +1,4 @@
-$ ballerina run http-client-connector.bal
+$ ballerina run http-client.bal
 GET request:
 {"args":{"test":"123"},"headers":{"host":"postman-echo.com","user-agent":"ballerina/0.92.1-SNAPSHOT","x-forwarded-port":"443","x-forwarded-proto":"https"},"url":"https://postman-echo.com/get?test=123"}
 
--- examples/https-listener-client/https-listener-client.sh
+++ examples/https-listener-client/https-listener-client.sh
@@ -1,4 +1,4 @@
-$ ballerina run https-server-client-connectors.bal
+$ ballerina run https-listener-client.bal
 ballerina: started HTTPS/WSS endpoint 0.0.0.0:9095
 Successful
 ballerina: stopped HTTPS/WSS endpoint 0.0.0.0:9095
